65df8d25e7cd50ae73fea8c83c40c3d60129eeb0
Fabiolaph <a23328061310457@cetis131.edu.mx>
2025-10-02 20:14:13 -0500

    Delete public directory

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const rows = document.querySelectorAll('tr.table-danger');
-  rows.forEach(row => {
-    alert(`Alerta: Préstamo vencido en fila: ${row.querySelector('td').textContent}`);
-  });
-});
